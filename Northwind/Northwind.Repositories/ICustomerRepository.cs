c315735ebe83c9e30d0bea5ee12ab584c388b277
CthulhuRise97 <42920098+CthulhuRise97@users.noreply.github.com>
2020-09-05 13:41:13 -0500

    Customer Repository
    Customer repository implemented from two interfaces

---
+++ Northwind/Northwind.Repositories/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Northwind.Models;
+using System.Collections.Generic;
+
+namespace Northwind.Repositories
+{
+    public interface ICustomerRepository:IRepository<Customer>
+    {
+        IEnumerable<Customer> CustomersPagedList(int page, int rows);
+    }
+}
